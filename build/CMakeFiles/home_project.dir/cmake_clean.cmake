9bf65c8b9def548fa3d61825801ceec11278274a
ishan <ishansheth@gmail.com>
2017-09-23 18:04:50 +0200

    .ADD cmake file to build the project

---
+++ build/CMakeFiles/home_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/home_project.dir/src/my_class.cpp.o"
+  "home_project.pdb"
+  "home_project"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/home_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
